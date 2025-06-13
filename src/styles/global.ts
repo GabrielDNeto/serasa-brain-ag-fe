993cec30c589c034517abef8f9201466cea31379
Gabriel Neto <gabrielneto.contato@gmail.com>
2025-06-13 16:56:33 -0300

    feat: setup styled components

---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Inter', sans-serif;
+    background-color: #f2f2f2;
+    color: #333;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
