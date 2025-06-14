d44cbe72f8ee7dfd1514a2b88c5c42886f1f060b
Gabriel Neto <gabrielneto.contato@gmail.com>
2025-06-13 21:03:35 -0300

    feat: routes guard

--- src/components/providers/RouteGuard/index.tsx
+++ src/components/providers/RouteGuard/index.tsx
@@ -1,7 +1,24 @@
-interface IRouteGuard {
-  children: React.ReactNode;
-}
+import { AuthStatusEnum } from "@/@types/auth";
+import { APP_ROUTES } from "@/config/routes/constants";
+import { useAuth } from "@/hooks/useAuth";
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router";
 
-export default function RouteGuard({ children }: IRouteGuard) {
-  return children;
+export default function RouteGuard() {
+  const { authStatus } = useAuth();
+
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (authStatus === AuthStatusEnum.UNNAUTHORIZED) {
+      navigate(APP_ROUTES.public.signin);
+    } else if (authStatus === AuthStatusEnum.AUTHORIZED) {
+      if (pathname.includes(APP_ROUTES.public.signin)) {
+        navigate(APP_ROUTES.private.dashboard);
+      }
+    }
+  }, [authStatus, navigate, pathname]);
+
+  return <Outlet />;
 }
--- src/config/routes/router.ts
+++ src/config/routes/router.ts
@@ -4,22 +4,28 @@ import PagesTemplate from "../../components/templates/PagesTemplate";
 import { APP_ROUTES } from "./constants";
 import Producers from "@/pages/producers";
 import Signin from "@/pages/signin";
+import RouteGuard from "@/components/providers/RouteGuard";
 
 export const ROUTER = createBrowserRouter([
   {
-    path: APP_ROUTES.public.signin,
-    Component: Signin,
-  },
-  {
-    Component: PagesTemplate,
+    Component: RouteGuard,
     children: [
       {
-        path: APP_ROUTES.private.dashboard,
-        Component: Dashboard,
+        path: APP_ROUTES.public.signin,
+        Component: Signin,
       },
       {
-        path: APP_ROUTES.private.producers,
-        Component: Producers,
+        Component: PagesTemplate,
+        children: [
+          {
+            path: APP_ROUTES.private.dashboard,
+            Component: Dashboard,
+          },
+          {
+            path: APP_ROUTES.private.producers,
+            Component: Producers,
+          },
+        ],
       },
     ],
   },
